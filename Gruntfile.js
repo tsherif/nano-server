17a81f8a6899f3415f98e615e85f50cd514ab2df
Tarek Sherif <tsherif@gmail.com>
2014-07-18 15:10:27 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  "use strict";
+  
+  grunt.initConfig({
+    jshint: {
+      options: {
+        eqeqeq: true,
+        undef: true,
+        unused: true,
+        strict: true,
+        indent: 2,
+        immed: true,
+        latedef: "nofunc",
+        newcap: true,
+        nonew: true,
+        trailing: true
+      },
+      all: {
+        options: {
+          node: true
+        },
+        src: ["simple-server.js",  "Gruntfile.js"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerTask("default", ["jshint"]);
+};
