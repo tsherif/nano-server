c8dcd96da5621d21353992f5ce2f5552e493f476
Tarek Sherif <tsherif@gmail.com>
2014-07-22 17:25:10 -0400

    Linted.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         options: {
           node: true
         },
-        src: ["nano-server.js",  "Gruntfile.js"]
+        src: ["nano-server",  "Gruntfile.js"]
       }
     }
   });
